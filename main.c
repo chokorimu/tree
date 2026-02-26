9fe02f17d1813e7e9ca78b6a7f318d912376346f
chokorimu <rezondavar@gmail.com>
2026-02-26 21:32:41 +0700

    implement search function

--- main.c
+++ main.c
@@ -8,41 +8,46 @@ struct node {
     struct node* right;
 };
 
+struct node** searchNode(struct node** root, int value) {
+    struct node** cursor = root;
+    while (*cursor != NULL) {
+        if (value == (*cursor)->value) {
+            return cursor;
+        }
+        if (value < (*cursor)->value) {
+            cursor = &((*cursor)->left); 
+        } else {
+            cursor = &((*cursor)->right);
+        }
+    }
+    return cursor; 
+}
+
+bool isNumberExist(struct node** root, int value) {
+    return *searchNode(root, value) != NULL;
+}
+
 void insertNode(struct node** root, int newValue) {
-    struct node* newNode = malloc(sizeof(*newNode));
+    struct node** leaf = searchNode(root, newValue);
+    if (*leaf != NULL) {
+        return;
+    }
+    struct node* newNode = malloc(sizeof(struct node));
     newNode->value = newValue;
     newNode->left = NULL;
     newNode->right = NULL;
 
-    if(*root == NULL) {
-        *root = newNode;
-        return;
-    }
-    struct node* cursor = *root;
-    while(true) {
-        if(newValue == cursor->value) {
-            free(newNode);
-            return;
-        }
-        if(newValue < cursor->value) {
-            if(cursor->left == NULL) { 
-                cursor->left = newNode;      
-                return;
-            } cursor=cursor->left;
-        }
-        else if(newValue > cursor->value) {
-            if(cursor->right == NULL) { 
-                cursor->right = newNode;
-                return;      
-            } cursor=cursor->right;
-        }
-    }
+    *leaf = newNode; 
 }
 
 int main() {
     struct node* root = NULL;
-    insertNode(&root, 5);
-    insertNode(&root, 6);
-    insertNode(&root, 6);
+    insertNode(&root, 40);
+    insertNode(&root, 20);
+    insertNode(&root, 10);
+    insertNode(&root, 30);
+    insertNode(&root, 50);
+    insertNode(&root, 60);
+    printf("%d\n", isNumberExist(&root, 50));
     return 0;
 }
